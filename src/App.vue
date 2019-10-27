7b115773b9a822fffc0e64b2a873a5e629a0e190
myprojectsfile <my.solutions.asset@gmail.com>
2019-10-28 00:31:32 +0330

    Instantiate counter component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Counter />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from "./components/Counter.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Counter
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Clicked {{ count }} times! Count is {{ parity }}.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data: function() {
+    return {
+      count: 0
+    };
+  },
+  computed: {
+    parity: function() {
+      return this.count % 2 === 0 ? "even" : "odd";
+    }
+  }
+};
+</script>
